420237f90c899857154688fd7647bb9a0471b5b0
Void Network <147660082+VoidNetworkOnTop@users.noreply.github.com>
2025-05-19 20:23:51 -0400

    Update games.js

--- static/assets/json/games.js
+++ static/assets/json/games.js
@@ -7,7 +7,7 @@ const gamesData = [
 {
     title: "Among us",
     imgSrc: "/img/gameimg/Amoungus.png",
-    link: "/local games/among-us/index.html"
+    link: "/local games/AmongUs/Among Us online.html"
 }, 
 {
     title: "Slow Roads",
