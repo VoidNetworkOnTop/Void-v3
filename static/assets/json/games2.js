e433391aa8614d2f215aa14f02249d28740987cc
Void Network <147660082+VoidNetworkOnTop@users.noreply.github.com>
2025-05-20 15:22:20 -0400

    Update games2.js

--- static/assets/json/games2.js
+++ static/assets/json/games2.js
@@ -2,7 +2,7 @@ const games2Data = [
 {
     title: "Geomatry Dash", 
     imgSrc: "https://lh7-us.googleusercontent.com/xzhOGM4wjIJKEp5B80TXpg4LArOEXtTVosOj98RMbNdzdtQBMitXKakY6kzTuaHJQoJa2TI--LQseSXSaGecSUUYRbACHQ3-M0R87sXCC_nWy0q0ovn8sXna5ZbHDSZKQBccX1w95MT5KwGdjoMm5wuTIKHiOw",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://geometrygame.org/game/geometry-dash-lite/")
+    link: "/local games/geodash/index.html"
 },
 {
     title: "Real Flight Sim", 
