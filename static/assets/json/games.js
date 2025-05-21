10751e9550678c775c38d53a20ba9b2dc699ddc7
Void Network <147660082+VoidNetworkOnTop@users.noreply.github.com>
2025-05-21 09:52:40 -0400

    Update games.js

--- static/assets/json/games.js
+++ static/assets/json/games.js
@@ -12,7 +12,7 @@ const gamesData = [
 {
     title: "Slow Roads",
     imgSrc: "/img/gameimg/SlowRoads.png",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://lizard-foil-pincushion.glitch.me") 
+    link: "/uv-encode.html?link=https://lizard-foil-pincushion.glitch.me"
 },
 {
     title: "Swoop",
