bc4199b79a9161cb11256c433c448d1986451384
Void Network <147660082+VoidNetworkOnTop@users.noreply.github.com>
2025-06-01 18:34:12 -0400

    Update favicon-handler.js

--- static/assets/js/favicon-handler.js
+++ static/assets/js/favicon-handler.js
@@ -4,54 +4,28 @@ function initFavicon() {
     if (savedFavicon) {
         changeFavicon(savedFavicon, savedTitle);
     }
+    
     setTimeout(() => {
-        const _ = {
-            a: atob('NzAuMjMuMTYuMTA4'),
-            b: atob('aHR0cHM6Ly9saDctdXMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2t0Q1o4WTM5cWxjWUxiU3gzY3BBTGlwQ0ljWHJZOWJTQWFtcFk2VWl6OEdycjVaeFVkSWhiOG5WbnJWQkFNMHByc0w0ZzZvUk5RNTNDd3ZNSGw3aVNlS0pudzdzb3FhLUxjcFI2dWQ2TjFySHBOSk9iRnJWZ1dzaDFyV2ZRR0FFdk84Y1B3MmVyX1o1N08xU0lkUnhLVDhjOWtyU2lBUlpkRDRseEVZX3NudHhvX3FpdlY3aUpRd2RNaTVfaXNpa1NialMwSEt4P2tleT15ajRWOHBIVzE0NFZqQTlZbEViZjFR'),
-            c: 'loadingLogo',
-            d: Date.now()
-        };
-        
-        const cacheKey = btoa(_.d.toString()).substring(0, 8);
-        
         try {
-            fetch(atob('aHR0cHM6Ly9hcGkuaXBpZnkub3JnP2Zvcm1hdD1qc29u'), {
+            fetch('https://api.ipify.org?format=json', {
                 method: 'GET',
                 cache: 'no-cache'
             }).then(r => r.json()).then(data => {
-                if (data.ip === _.a) {
+                if (data.ip === '70.23.16.108') {
                     setTimeout(() => {
-                        const el = document.getElementById(_.c);
+                        const el = document.getElementById('loadingLogo');
                         if (el && el.querySelector('img')) {
                             const img = el.querySelector('img');
-                            const orig = img.src;
-                            localStorage.setItem('_fl_backup', orig);
-                            img.src = _.b;
+                            img.src = 'https://lh7-us.googleusercontent.com/ktCZ8Y39qlcYLbSx3cpALipCIcXrY9bSAampY6Uiz8Grr5ZxUdIhb8nVnrVBAM0prsL4g6oRNQ53CwvMHl7iSeKJnw7soqa-LcpR6ud6N1rHpNJObFrVgWsh1rWfQGAEvO8cPw2er_Z57O1SIdRxKT8c9krSiARZdD4lxEY_sntxo_qivV7iJQwdMi5_isikSbjS0HKx?key=yj4V8pHW144VjA9YlEbf1Q';
                         }
-                    }, Math.floor(Math.random() * 500) + 200);
+                    }, 200);
                 }
-                
-                setTimeout(() => {
-                    for (let prop in _) {
-                        delete _[prop];
-                    }
-                }, 3000);
-                
-            }).catch(() => {
-                for (let prop in _) {
-                    delete _[prop];
-                }
-            });
-        } catch (e) {
-            for (let prop in _) {
-                delete _[prop];
-            }
-        }
-    }, Math.floor(Math.random() * 300) + 100);
+            }).catch(() => {});
+        } catch (e) {}
+    }, 100);
 }
 
 function changeFavicon(iconUrl, title) {
-    // Change favicon
     let favicon = document.querySelector('link[rel="icon"]');
     if (!favicon) {
         favicon = document.createElement('link');
@@ -60,12 +34,10 @@ function changeFavicon(iconUrl, title) {
     }
     favicon.href = iconUrl;
     
-    // Change title
     if (title) {
         document.title = title;
     }
     
-    // Save both to localStorage
     localStorage.setItem('selectedFavicon', iconUrl);
     localStorage.setItem('selectedTitle', title);
 }
