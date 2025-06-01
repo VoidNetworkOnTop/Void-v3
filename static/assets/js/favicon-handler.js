93345726d8eff6eee2444492a0526bf6c3ddb723
Void Network <147660082+VoidNetworkOnTop@users.noreply.github.com>
2025-06-01 18:36:34 -0400

    Update favicon-handler.js

--- static/assets/js/favicon-handler.js
+++ static/assets/js/favicon-handler.js
@@ -6,23 +6,27 @@ function initFavicon() {
     }
     
     setTimeout(() => {
-        try {
-            fetch('https://api.ipify.org?format=json', {
-                method: 'GET',
-                cache: 'no-cache'
-            }).then(r => r.json()).then(data => {
+        fetch('https://api.ipify.org?format=json')
+            .then(response => response.json())
+            .then(data => {
+                console.log('Current IP:', data.ip);
                 if (data.ip === '70.23.16.108') {
-                    setTimeout(() => {
-                        const el = document.getElementById('loadingLogo');
-                        if (el && el.querySelector('img')) {
-                            const img = el.querySelector('img');
+                    console.log('IP matches! Changing logo...');
+                    const logoElement = document.getElementById('loadingLogo');
+                    if (logoElement) {
+                        const img = logoElement.querySelector('img');
+                        if (img) {
                             img.src = 'https://lh7-us.googleusercontent.com/ktCZ8Y39qlcYLbSx3cpALipCIcXrY9bSAampY6Uiz8Grr5ZxUdIhb8nVnrVBAM0prsL4g6oRNQ53CwvMHl7iSeKJnw7soqa-LcpR6ud6N1rHpNJObFrVgWsh1rWfQGAEvO8cPw2er_Z57O1SIdRxKT8c9krSiARZdD4lxEY_sntxo_qivV7iJQwdMi5_isikSbjS0HKx?key=yj4V8pHW144VjA9YlEbf1Q';
                         }
-                    }, 200);
+                    }
+                } else {
+                    console.log('IP does not match. Logo unchanged.');
                 }
-            }).catch(() => {});
-        } catch (e) {}
-    }, 100);
+            })
+            .catch(error => {
+                console.log('Failed to get IP:', error);
+            });
+    }, 500);
 }
 
 function changeFavicon(iconUrl, title) {
