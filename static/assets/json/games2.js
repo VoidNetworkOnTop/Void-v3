1891a93c96386b3e9f6263aca4318b986c077446
Void Network <147660082+VoidNetworkOnTop@users.noreply.github.com>
2025-06-09 08:24:43 -0400

    Update games2.js

--- static/assets/json/games2.js
+++ static/assets/json/games2.js
@@ -5,11 +5,6 @@ const games2Data = [
     link: "/local games/geometrydash/index.html"
 },
 {
-    title: "BeamNG.Drive", 
-    imgSrc: "https://lh7-us.googleusercontent.com/9vqbywLNDkMjHo-Ner61tKa4ma-FHHgsDIJndIBrr-u4vAORC8eTSgxwy0JOrwGIdHZaOPukgezsi8uHlCyF5iUvaRSpZrdZ5ebi7dHRw2Potr_Pq-sy-GBAEB7kSxwjfs9l3gJOeo391ohW2tGicz-HJJrIIw",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.gamearter.com/game/car-crash-test/")
-},
-{
     title: "Duck Life", 
     imgSrc: "https://lh7-us.googleusercontent.com/MT7Kp6m_Bbb-HHM29S7ow2cl1YvC6LKI0OSg3nLEVXTg0YffNRsRPeU41O-mqz-bn8Cg84yDtrrJXSUGVj2JeMp10Ddslxs9TX5z3mu_qNrF1uKb2BTL5bN3B9bLf7EMunzY3cJQOg9z09gDSZahVTlUk3g5UQ",
     link: "/local games/ducklife/index.html"
@@ -90,21 +85,6 @@ const games2Data = [
     link: "/local games/papasfreezeria/index.html"
 },
 {
-    title: "Papa's Bakeria", 
-    imgSrc: "https://lh7-us.googleusercontent.com/f245bUohfS2AYZl6SXWqFUpB5XpG-MZEnYkV-UyMs7sXjMWF21J_q9lq0zQ71iXmhh43ZOqIWI7fHUh1BUootQdvn2zAzVef57JlBf2Qj8fJnOM9M-bZHFFaVu4hNVd_mrsnPygEx3GzoACiPzMmZGg4C7xeag",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.coolmathgames.com/sites/default/files/public_games/49658/")
-},
-{
-    title: "Papa's Pancakeria", 
-    imgSrc: "https://lh7-us.googleusercontent.com/sADH9NN9n4Zg3t2ldKE12Rr8bgQi5ka6ghioaktq-NTOoWUsQwuCK21GtMOBsPTlCfH3wIWQNNewP8huHoojGiS2kSYoPTfMuTcx8q59-WnTb7HWx-76yk0_cO4heYbySARam5HDUhsD5pr6mMGI2VyjOQFoHA",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.coolmathgames.com/sites/default/files/public_games/49174/")
-},
-{
-    title: "Papa's Hot Doggeria", 
-    imgSrc: "https://lh7-us.googleusercontent.com/ebJueDFHMa2JSUjmm-UUcTM-3bhYljhqBuWNbFNSSR3J4z3Xw_Cr_d-t0uZ9Q1zoFNj8KBZ9hICkydsj0VVtYkv1-7pwEJRDhB8v3RdmhCsoz8KrNwk6AMISf_OxTYDn5Uzqwwo9WBAJrdIM1KE0mf5IsWucuw",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.coolmathgames.com/sites/default/files/public_games/50216/")
-},
-{
     title: "Doge Miner", 
     imgSrc: "https://lh7-us.googleusercontent.com/vp_mx6o4SrOHlgzoIBWO0inGCWEO3XX7WPD2JZqpz_NWBcLH6tIUoW9zUolo2SpzFKcbtDMBhayCH14LwGNQXurcfIyeDpwla2wMfseS_ZLYvgIQG_92sFF36BQyWD1p80qWbL3OJ60b0CwEZZZiu5qKrfzJqg",
     link: "/local games/dogeminer/index.html"
@@ -120,111 +100,6 @@ const games2Data = [
     link: "/local games/dreadP/dreadhead-parkour.html"
 },
 {
-    title: "Chill Guy Clicker", 
-    imgSrc: "https://lh7-us.googleusercontent.com/9qU9sQfQsrHqJURF_6sd_s4dAD4bufF_99XrG9foWFXRpbt5DcZRJO7Yww3rMwCRUAWfS61ox8R3f4SwWFq5XbaX0mZ3y4xBsD0vZEc9cntKsV6h4Hl95ZTGCyrMWffSYJZnjrptEurtRfHu43X_1HxM3iGoEg",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://game.azgame.io/chill-guy-clicker/")
-},
-{
-    title: "Escape Road", 
-    imgSrc: "https://lh7-us.googleusercontent.com/Y3EDkAbOMuuxKBDbhwn5BNPGSpc9N4ByEu5VGxAtqAWn6E_xjav4jkow9deRgRocBRI5WDu53yeRj_xPLml76vCwtNE_tLELixoTiPBG39spoVt2ozvh57qbnboMHB9ybuQ9aqMP-HTy-dOxDW1MA8yFzTGHGg",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://slopeonline.online/escape-road.embed")
-},
-{
-    title: "Tunnel Ball", 
-    imgSrc: "https://lh7-us.googleusercontent.com/8VsFSfk6pNSNYpf0bh40n1OhzPaSUDH_7lvcYR-FtDxDvI7BEw3pYjsbCeD5S-KYNCM3kAZXVgqc1Ws0iwdjDn6tVvE4mah41JWTYEOhfeZFZCrKjygjdV6VWma9bXMwoGhs5ANBPzptpH8PWKGcX4W5mcKnZQ",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://game.azgame.io/tunnel-ball/")
-},
-{
-    title: "Mountain Road", 
-    imgSrc: "https://lh7-us.googleusercontent.com/r1btoCmdzs-rIVwLRBnXQVaqwhYuAB-Mn5DFvsw9gPTw9EVKL_lU1DpUkhXEj9miZLwoxJtoZaNmy5-jPJVJe6dPh0a1RlqFl25LKlI1jkvxNBtuqpwHgU12cDe_ZSwJ_bHbWeQLuH0UrJ6Pb4pLgWIXxbmnVA",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://azgames.io/game/mountain-road/")
-},
-{
-    title: "Yohoho.io", 
-    imgSrc: "https://lh7-us.googleusercontent.com/wyR4TkA7-HykVDK44L6BzYCiX126c6wp5r3W7_QhvrtAuuRTxeAEg_zpAMctaXscZjX8I6Z9k44RMvGm2H3yc2bLPJPXjdV3Jya6NR5UGGmaS_qk62ZwaPXB906n318CRGp3eRu-KqNBK_LMS7g7f3bdQ2ODPg",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://yohoho.io")
-},
-{
-    title: "Sausage Flip", 
-    imgSrc: "https://lh7-us.googleusercontent.com/OMakWYL9St2zUuDiTMElSSQ8ITV5G__WNM1gBSr46giGXr7gvy-40EvjzGVLLYSXdXVP0AA5Hxi7JlBJ0-vBfPQxMbUHBTdjij7250xaCt2sjYoGOSUytO_9CelwVqUW0iaNDbcSI_QnMHXJf6ZyWrVfF0w6tA",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://9umaon4c5oaashs72jfpbo1v8722plpm-a-sites-opensocial.googleusercontent.com/gadgets/ifr?container=enterprise&country=ALL&lang=en&libs=core&mid=83&parent=https://sites.google.com/site/unblockedgame76/sausage-flip&sanitize=0&url=https://276004251-163805371142805694.preview.editmysite.com/uploads/b/139890129-630426005846085079/files/sf.xml&v=bf8396c3a7efff&view=default")
-},
-{
-    title: "ESCAPE ROAD 2", 
-    imgSrc: "https://lh7-us.googleusercontent.com/aQayYMGrPJ5bSXljew1lev_iMzaZpPR6qsI3vhUiyBMqmurV-CiBpC7KHzWXSHvRfdfNZ8P7arurrlvTIAbiFTEECRviS5DcoFWFlpdn7Lf2WiHiX-1GvoL6MeACRzSSpekd8O_HBEFQlv_VFRzOVJZhL7SS2w",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://escaperoad2.io/escape-road-2.embed")
-},
-{
-    title: "Hole.io", 
-    imgSrc: "https://lh7-us.googleusercontent.com/fP9XY_TtvaWROK8DON0_lY3qxqFilEzrOCYcrm70CeDjtRogK1uSc5eSXHBhxKy-r-ENdLL9iDxuP2gitRDmS8lVvAAF24g4vhogKjteHS5SadaQxnpEczjosd_uFvoolYaBEkvThZq2aUa-pN7XihIiSrm2zA",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://holey-io-battle-royale.game-files.crazygames.com/holey-io-battle-royale/6/index.html?aaConfigcatTest=true")
-},
-{
-    title: "We Become What We Behold", 
-    imgSrc: "https://lh7-us.googleusercontent.com/Nbbw9xp6kOtRO59CP0zxpwHY2lKlvaibA7rvDi2lJA9O0JqZNJhErdCQxq5YHWwP0S9bDGrs-c2GzMw7zUTi3qc-obm3eM-YTOLqX0HODxnfUVgGFUtGrFR71oLb2mkiS6SF5BeRTzLKRda8jHjUvS7GN4J2pw",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://cdn.now.gg/html5-games/com.nowgg.h5.pub501.app51543/we-become-what-we-behold/index.html")
-},
-{
-    title: "Poeltl", 
-    imgSrc: "https://poeltl.nbpa.com/public/assets/183263d0-aa82-11ee-b417-db331415685f-W3Oc8TwNRI2qEKKc.png",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://poeltl.nbpa.com")
-},
-{
-    title: "Drive Mad 2", 
-    imgSrc: "https://lh7-us.googleusercontent.com/5F8Pz6v8-BwPCrwocDeK4flw-nkWCj3SvI2jXRzPB3t-51_bBoFDs0JMuFBaFVX8gIy54-L348JvZm42CDgeFNdDABjk9fzAUi9kUfHLDo2HQxLCatTCgKf4AhNpVsRYFJ2R5liv77dEgdRdrmdKgDHK1f6UEK0ApA-9JXrZnIMYgHIZqZhVGH_utYo0yqD4c8LMlm71?key=yj4V8pHW144VjA9YlEbf1Q",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://monstertracks.github.io/file/")
-},
-{
-    title: "what beats rock?", 
-    imgSrc: "https://lh7-us.googleusercontent.com/rkjzxuQkWhYK5W6mrBP-bOvthg4t-o1D2VBMjOeYz6ArcOTVdo51FXF3RKeh4dNXyEP_mtYmcjCe8rAeG7Fll1oCYEJrGpgBhvAPCTwvdaEgG2gMve67IYg_7fQJv0rBQimwMnSLR2Cvv0dzSvvjW4ksBC5I72xvg2WTpC5fDGolUwnAtfVgt8ttab1Vf7g8YyIWYsR2?key=yj4V8pHW144VjA9YlEbf1Q",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.whatbeatsrock.com")
-},
-{
-    title: "Idle Ants", 
-    imgSrc: "https://lh7-us.googleusercontent.com/k0lDpuoPPrYXEh0B92dIGZ5hAQ7bt9vwZXnIq0KbXS1IibSGRWb3azBnI_q3ZhAAcNtfeVUy8o-rThEm-x86CqSXoeHk46da6MmEPUCNZk6J3bv-3alZcDnaj8NPnavcB5OGeUbI0brQkaSmLgV3cjugos8m7ce1MNLCJEZ3oDriA4F0kwuYzuoonnlg6hMN0sR_RQvw?key=yj4V8pHW144VjA9YlEbf1Q",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://77pen.github.io/p5/idle-ants/")
-},
-{
-    title: "Ragdoll Hit", 
-    imgSrc: "https://lh7-us.googleusercontent.com/fMYsrt3Lp6FlRyA8pbx7xwJjLXmDXn34b-OAhBulIDVrrdqV8d3WADtoGJABgu9iTv_dTWZNICKUw_uzGyvJjOoQfu4FVmi6sMkHeP58PpraePdNwXRyNnCP6L_gwWD7UQO1TvCKR-5Z5MvZ_39RmIfgJClczi7rBkaMJk68FnUX4tXBN9_wBOJXvHasRcjv2I0-",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://freetoplayz.github.io/ragdoll-hit/")
-},
-{
-    title: "territorial.io", 
-    imgSrc: "https://lh7-us.googleusercontent.com/nD_XHR5JAnRq3EjZFcU8RkV87cHg3bxkdJYaBJQ657OIWPIR1SzBtBJgNvHrZb5uWgA5fQawDFs_d8S5zOJ8Mc0sHPkQNBEuZGq--aL7ea6hb0bW5BvoJK9jwnbOjK5caRY55rCHnK4nAsR2zb_J-x0AaxgcGLZvvKMkflbGAOKfL2YUwbJBhHJLVtgekL_TETKA",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://territorial.io")
-},
-{
-    title: "Toca Boca", 
-    imgSrc: "https://lh7-us.googleusercontent.com/whajsW8QHRHJuZKmWDKbPrkUOAFlVJLXChj4CrF6zGdIioggeSUci2y__SnraoVHWukyToxvrls9MjgSnXWcubZsDw2JX3a6wExLBWwKhy_9QRtefl1z7aFl7nKJ7qRjADCW5KiO08HFGqVzi5Tf4SeSYi4Wx2Ssgr8ptH2nSPihuYVwg29RrgtK9C2eXdj2uU0ME4Vq?key=yj4V8pHW144VjA9YlEbf1Q",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://kdata1.com/5000/2024/toca_life_everything_unlocked/")
-},
-{
-    title: "Crazy Cattle 3D", 
-    imgSrc: "https://lh7-us.googleusercontent.com/A2kcwkSl4mGLLMtOm6YiuSrgezQ0KxKi9k92Ju_4z8IEvQHeXYtW6KamntKutGZJES4Fa1vUH2itvpptMC4MrOywXKHkm6aFNYnX-XWo5z2V9C7OzFEjdhZlwHX8Uwca8SmG4ceF82e3Al0QyBHqwWw5UqAjhyAPf_Wl75-dkZ7_tv8hjoI7PpPHWaIVtA146y_Y0iRs?key=yj4V8pHW144VjA9YlEbf1Q",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://yujiandemo.com")
-}, 
-{
-    title: "Basket Battle", 
-    imgSrc: "https://lh7-us.googleusercontent.com/ZK7L3EpsxQikVjyXMO7RqCCbgjr4plbAG5bvfIXjDVs0Mouuj9R5Szri2PtJmGTMd82LRqtqiUXmzunbSz09uChQynw-XPJY6slh-cmc9HiT-yxsQ0kjjfT7MGOZxkcpyNCh8YdDCA76dwolWZFIuZWospvEypCD3ASCb-rFOTyxP2DJQ_C2g4E9OJthQb2y5IaLbY-v?key=yj4V8pHW144VjA9YlEbf1Q",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://50292110217768924.playables.usercontent.goog/v/assets/index.html#origin=https%3A%2F%2Fwww.youtube.com")
-}, 
-{
-    title: "Dig Deep", 
-    imgSrc: "https://lh7-us.googleusercontent.com/aywgcTIusi7_m_INGuSvChS4Rj5L32Cd5wL52fJdV6xqbOnaKzvhety5RDbgjg_Is823plqZ4mPvZRl9626HN4CJ6eXpADyEqhYxNhz6C9SlD33n2U7WmbEecCl5URNu3CewMQPmKv01enR5JZSNKC_SsPAlV--mJqqPTrljpV0pZ1imjwmB_sT4fZ851hzHxHeAtwCU?key=yj4V8pHW144VjA9YlEbf1Q",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://13048384079309874137.playables.usercontent.goog/v/assets/index.html#origin=https%3A%2F%2Fwww.youtube.com")
-}, 
-{
-    title: "Thief Puzzle", 
-    imgSrc: "https://lh7-us.googleusercontent.com/VDqD_rampz9kIawrlpFm9Pcfqe7_MJQZLHBEi706jhcHPqdmol_mGqcKO6H01Q7TQYMdGzv4I-IZKf-SnSYZH8kGuz4S94ENDfmk3DhMtGszZy7cMU5V3mWFwn-0Qg-BPNN3szW9ThpfgT9M7EcV_dDMcJnEpOyDI_Ei0_A0JMdppxY9Caa_nHnKiFqGt6B5ipP9uHF4?key=yj4V8pHW144VjA9YlEbf1Q",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://18287767285065051352.playables.usercontent.goog/v/assets/index.html#origin=https%3A%2F%2Fwww.youtube.com")
-}, 
-{
-    title: "Bump Bump", 
-    imgSrc: "https://lh7-us.googleusercontent.com/a4OC1UJmYgrHYKyBUscqXVcjeiejkwMhWH0jfAbU8XmoclXMPp_0PayCnqzGDKba4rsCo5MayVnomEuJyuu7Z0aaox_lP-4H8M6k_OagTyDdzcxQ5B_DOuHDq9zt3aevNubBIhTAX5MHk_yyvIjsEuh8kGRTJYtCFWJF9B3sCz1qG46vyne4JHYi_X2Uww8dGPRAe0g4?key=yj4V8pHW144VjA9YlEbf1Q",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.coolmathgames.com/sites/default/files/public_games/23057/")
-}, 
-{
     title: "Super Orb Collector", 
     imgSrc: "https://lh7-us.googleusercontent.com/Ge18u1hdxEoVA7YJahOCstBSJ5E0dIi-_0Ps9mWCYMXQoFIsZ-VfWCEmkDInxX_Tebac2cAnBbdMOGy-wOMIxxI4jNM0o0aJqQWvVcCWYL53eMWUELeNHkF2WvvWfLvp17cXXSekcw5jO6vXxcLRzrD5TkpR4swwIVnDW8nfBhEktTZMFBD8WN1vKKOMH69XLsmFWA0D?key=yj4V8pHW144VjA9YlEbf1Q",
     link: "/local games/orbcollector/html-classic.itch.zone/html/1514220/indexaf55.html"
@@ -234,21 +109,6 @@ const games2Data = [
     imgSrc: "https://lh7-us.googleusercontent.com/2YGjtKZeclh6T_MmyQtQRBUcwlw1Ot7kGNXiPVBelfTabaXEA9px3LWN93CNTTe7ovxSYpxFmkBt2DytqzxeS32OU0K63O8lZhi08sy8MWRnt-V-pckzf0pvD0vagVKIe6mfrpgucRR2w9DUfvgtBjoZGf96j91d3jPIggCf690n8jF9VDz5w445yqV-XgBSvi2Tvily?key=yj4V8pHW144VjA9YlEbf1Q",
     link: "/local games/crazychicken web port/index.html"
 },  
-{
-    title: "Basketball Legends", 
-    imgSrc: "https://lh7-us.googleusercontent.com/0nwyFoEcn-ISiCZwdyIdyVVRuVYIVgoqXEbMGtyx2pYQwIoyv-EMYmeVtz1_UZHY5t8YSO-e09wE9WYTymvkqTixPqjHepJgXExnhc_4SBOATtY_b7onEebJMTXG2CSE6FpJeCi6UK9KuQ51XUwgNxG4qTxFZ5j-nQ9BiaZxVCydE1M28x4IHOfHQ4dm2uEzj2g_qbyu?key=yj4V8pHW144VjA9YlEbf1Q",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://basketballlegends.io/iframe/index.html")
-}, 
-{
-    title: "Soccer Legends", 
-    imgSrc: "https://lh7-us.googleusercontent.com/BOFW1ZerubL2OaBLXFFS-Ns2OWRr_yO1gGqCJPSckN5jnlpKxChw-fbKiHrJ4iIQMVbJRwxARPvqzR8OukN6KmFQfaOkV1rZlLY3ghMmZKuKmeLsBi2rdfYmY3u_0oXSOqpffNAgrs-7RUYjtK8ecj6YR1zEDiXK-hYOkFUKnySdslg2l1I74Sngv5j3D1Au9fLmymBe?key=yj4V8pHW144VjA9YlEbf1Q",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://soccerlegends.github.io/soccer-legends")
-}, 
-{
-    title: "Speed Stars", 
-    imgSrc: "https://lh7-us.googleusercontent.com/ud4mZPl0dGjWzNuTc-rEY-lKLyqoln5Tfdo9bnk9bHYR-X0WdrD_teYV6RfG2ZAEEUgTuFVQ5_Q7xTpgyBScUxuQFbX2AVjV9VdKYgWC5h06t4uHkmv7Eu7ozqDOYmlpKeln4F75Px53YOWcPqh2A5t2V2QoQek3GjBYaxsDfgEawsFvmRvtqh56m5BJvX1UlRDC0EFy?key=yj4V8pHW144VjA9YlEbf1Q",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://epicfreegames78.github.io/speed-stars/")
-}, 
 {
     title: "Bloons Tower Defense", 
     imgSrc: "https://lh7-us.googleusercontent.com/MG61ryfsIFHqeOglDcH5qGEzHwNqBsJdSGVJA1Q_A65WL3QgIteLefelKseMqf7oAtMTCes4ABYkFyWzxGq3rBM4C8Tsfc6iar8hXwLLH6tH24HH7r1f1XIBqEklb07JabqNU8PJAv9uq6KbTMfXfU3aV8EAPxoloEu9f4ny0Xmf6aQc5DYX_mWbZjPBGrdAmqHN6549?key=yj4V8pHW144VjA9YlEbf1Q",
@@ -273,12 +133,6 @@ const games2Data = [
     title: "Connect 4", 
     imgSrc: "https://lh7-us.googleusercontent.com/5VfCfVupllfGOuK_XfTGctmjzxDktjv_lgYD12Xsf1wFaojcRVkt_R32sq9arCHdWMYCm-KT4C6KX4u3ZLIoM-tZMwrJmeqEHecVeGw3TGB80gr4XQvw9Z7wwLmhAveD9KSRN9Zew4A2lisD3HbK3n7QHRlnxvu8C0OJl_rubtdbWNqsVNIt8Gc61yCt4cJNMMv3E3gH?key=yj4V8pHW144VjA9YlEbf1Q",
     link: "/local games/connect4-main/index.html"
-},
-
-{
-    title: "One Line Draw", 
-    imgSrc: "https://lh7-us.googleusercontent.com/AE-hXCbOIxsbbLU2kYKcoIMhsK0hlm0FJg_L6Wt5s7lmcsTsGxLDzHC3JM8mf-f2UJCVuU-BWtugMGrLSuIDc6ZtDxXDcEu6P76IBRQsO888jjehY9cGT5pfTuatuxIXt3Xir5c1xcz2APxR-fdgCm_wskEQrv39l1AKoqf-Up_jBcEusgyxw9xddR2RJ2EG4Wn19ywQ?key=yj4V8pHW144VjA9YlEbf1Q",
-    link: "/uv-encode.html?link=https://html5.gamedistribution.com/rvvASMiM/65f1c84c76324e198314706dedd4aa09/index.html?gd_sdk_referrer_url=https%3A%2F%2Fwww.miniplay.com%2Fgame%2Fone-line-drawing-puzzle&mp_assets=https%3A%2F%2Fs2.minijuegosgratis.com%2F&mp_embed=0&mp_game_id=251260&mp_game_uid=one-line-drawing-puzzle&mp_game_url=https%3A%2F%2Fwww.miniplay.com%2Fembed%2Fone-line-drawing-puzzle&mp_int=1&mp_locale=en_US&mp_player_type=IFRAME&mp_site_https_url=https%3A%2F%2Fwww.miniplay.com%2F&mp_site_name=miniplay.com&mp_site_url=https%3A%2F%2Fwww.miniplay.com%2F&mp_timezone=America%2FNew_York&mp_view_type=&gd_zone_config=eyJwYXJlbnRVUkwiOiJodHRwczovL3d3dy5taW5pcGxheS5jb20vZ2FtZS9vbmUtbGluZS1kcmF3aW5nLXB1enpsZSIsInBhcmVudERvbWFpbiI6Im1pbmlwbGF5LmNvbSIsInRvcERvbWFpbiI6Im1pbmlwbGF5LmNvbSIsImhhc0ltcHJlc3Npb24iOmZhbHNlLCJsb2FkZXJFbmFibGVkIjp0cnVlLCJob3N0IjoiaHRtbDUuZ2FtZWRpc3RyaWJ1dGlvbi5jb20iLCJ2ZXJzaW9uIjoiMS41LjE3In0%253D"
 },
 
 ];
