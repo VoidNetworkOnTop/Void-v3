4aeacd5298158460404b2cd22b1c2553cfab35a5
Void Network <147660082+VoidNetworkOnTop@users.noreply.github.com>
2025-06-10 07:16:02 -0400

    Update games.js

--- static/assets/json/games.js
+++ static/assets/json/games.js
@@ -286,24 +286,9 @@ const gamesData = [
     link: "/local games/badtimesimulator/index.html"
 },
 {
-    title: "PokeRogue", 
-    imgSrc: "https://lh7-us.googleusercontent.com/EO6IQPt2BjmQUAfEJRveuF1rVXvNp6SW5c0xME3_kbA6U-v9JJ63OWKarSMv7DHfUzQ7Il6lRwge-BTlAvPsfPb-Y-F9Jc4RbzvhJRAH3BqPihfo95T4WpaJSIinkaCFP9UDDfsIe8uL6-7tpqRA8ix6yxr-XA",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://playpokerogue.com/play/")
-},
-{
-    title: "Plants VS Zombies", 
-    imgSrc: "https://lh7-us.googleusercontent.com/R9y7QmVMLl3g-_BHLhGMjHs82fxuwvNogfkjXJB9I8UGU-6ZTpWWzJvC2Yw2BbkPdg-w6q_0qJNnxBXDLK7gQ2vqjefHIvxAdQBd_i9Gapv0572DExY27VkTjvK8iFXbRK7BtlzJqnZwyBwNLd92E9zdhZpNaQ",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://en.gameslol.net/data/pvz/en/index.php")
-},
-{
     title: "Getaway Sho</x>otout", 
     imgSrc: "https://lh7-us.googleusercontent.com/fxmVYqxAHuq9zf7lE33Yy0nLKNiaChEmbEpfzm1MN_SyN24JPFaqDJU_nrEE5WmG4Qt8gPNUbLOI_UnrTwMfD1rKa7BF99M5sp6Z-VUeRoRXrJpgVfaOaUHH2r4iSt1XGWsH31iq_KDkuoUGxuFoxvkcJGadiA",
     link: "/local games/getawayshooter/index.html"
-},
-{
-    title: "Granny", 
-    imgSrc: "https://lh7-us.googleusercontent.com/HQLwOwNmWh8FX0xH9vT4nszydv9DDvXb-S4jDikjl4Rc0Vuk7UL2q3kgvy0_xxCB-xYqwle-mujKVKoyNitFdejUn3XjokEzT-j4nwe1TEzjSUhxHNKurf5M_tHmV4Dl9X5PMaKAblCZf7vIMYYAdrJ4dqD2lA",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://gnhustgames.github.io/granny/")
 },
 {
     title: "2048 Cupcakes", 
@@ -311,69 +296,9 @@ const gamesData = [
     link: "/local games/2048cup/index.html"
 },
 {
-    title: "Highway Traffic", 
-    imgSrc: "https://lh7-us.googleusercontent.com/P__Aavy47cuBl57H_MUwrAux8deSZlojzdCjoqaZo-1d2j752vMPjLXkIDKB1KsqkXXhxMRQ4gwKobr2xOcSCDhvlBaYemqH2bQGbJyrQCKisMCZTJZdlE99s-vz9E9OZKNPAcChF0PfgPF08jcM-I__ko9Ivw",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://unblocked-games.s3.amazonaws.com/games/2022/unity/highway-traffic/index.html")
-},
-{
-    title: "Cannon Balls 3D", 
-    imgSrc: "https://lh7-us.googleusercontent.com/JVoSv7MAeIBWYVblhbvFDEnlIn74qiVsmF-EJpQ07eCSr8H3XG7wX1CF7Xv9Y0o5roHvZ8l0TSz8kfuUQGsDJEiuC2e9ye4Uzkzk_LUewTddYFWHahQXpTEzNy6ajHitgTTPlKAbWXTEAmxsaG5QCCF7qFFyQw",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://cannonballs3d.h5games.usercontent.goog/v/004m7ejm69rs0/?origin=https%3A%2F%2Fgamesnacks.com&gameCenterId=gamesnacks&userActivityMetrics=true&eids=95322736%2C95336219%2C95344323&features=GameRendering__support_offline_feature%2CInterstitialFreqCap__block_pga_1m_interstitials%2CInterstitialFreqCap__freq_cap_60s%2CMonetization__populate_google_global_correlator#pc=3100681022882577&preStart=1732155158524")
-},
-{
-    title: "Words of Wonders", 
-    imgSrc: "https://lh7-us.googleusercontent.com/72vShQ_YwHv7JCAH_6Ah8t_pxX4eW6bMlxj_FxqOiMMu0lfVsEMUBx59JP_EuUPfHhiqBGjytYgntnmzZpx0ZSeKKtO4aII6oFmdhNz1GUh4Pnjte3abNRpucypQlGjWtKe4tdSIZ1ar2HRqzYSnGNuQljgrZQ",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://play.famobi.com/words-of-wonders")
-},
-{
-    title: "Sort It", 
-    imgSrc: "https://lh7-us.googleusercontent.com/cO-4CE-Op87eVufXyUOPuOI9-hkzFJeZ6TqCegjE86_XWuV1VcOju4mu4E4wuyBptoAq_z86rwT_oR-pKnfqYJwgasIS8SGY1emGMolhC6wL5weWj5aHsv2fbg6IbZ8kX-DrOfksyBIFN6s8qA0biHrJGuI1OA",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://play.famobi.com/sort-it")
-},
-{
-    title: "Lawn Mower", 
-    imgSrc: "https://lh7-us.googleusercontent.com/l0qEk8lmntNBl42FKtDngTCS9o_gKUICZBsaHRd_5yFhglvqUOzGJxKAoqkLQJea6-mffba_mFg9KNH2XCqUVoVvMmB65DQBQkDW-cSQT9Uc-VyQVYaPgQON26iUJHt-ZAZFybf9aGlB1l_BL4xNtDcVc70BHw",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://play.famobi.com/lawn-mower")
-},
-{
-    title: "House of Hazards", 
-    imgSrc: "https://lh7-us.googleusercontent.com/6fQmRC7Le6oqmBNIPFBW5QKyRvKE4qubVXlPU1s1lhM3vR32I75hUcCwSs32NpHdHq4YafutCj2KXAXGluJLzPgzdD1Adbsz8-UF27_ymBCMgLwuSQWI6q-0ETJL28B_KaEbsKeoLdQZ6h-akidcb2GSv09xgw",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://houseof-hazards.github.io/file/")
-},
-{
-    title: "Luminara", 
-    imgSrc: "https://lh7-us.googleusercontent.com/nNrg6GxKwrBEt0zQD8gUaVAWdonspAuISB13g4KVf34KRwRCnyaDNbUpeCuKqt_BaFT-59kfz_ApuYaxexvbnRNksYyY18WhOXTD98TOgogEKsOR8R6ZodFVPfgiAXAYBDc3X1aag5KpNZx0GttW7RJgycD-Cg",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.cbc.ca/kidscbc2/content/games/luminara/index.html")
-},
-{
-    title: "All Flee", 
-    imgSrc: "https://lh7-us.googleusercontent.com/tkYyliLPxqGQjdWSKiG5uWo2DWpKUK57izQBT0Proj3M58ReyUzGg47uGMH0hPFv1Jg2ZapPyPjz1h3X9Lcm0k3E3I2mtpJVvU2OUYx6Bv7_N7mutqkZfxlO066eA-N5_MRbcCMIP7Kl2pI8DedPhvdTOEQB3A",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.cbc.ca/kidscbc2/content/games/all-flee/index.html")
-},
-{
-    title: "Madalin Stunt Cars 2", 
-    imgSrc: "https://lh7-us.googleusercontent.com/DZ8CBHVobl4YY5w1St5PDjbszkyrQ5FX-TJwFoGr8BQ16aa_a_u6e7nCuPpPxsVxxRcBniPlCUhQjkqXsk_aEJYNohfbEfP3k8NYQxDdI5w3w8Gme9CavLBJYeR9tr_dXiW1rFXn7RwRCA3g5B0OHRo41EemzQ",
-    link: "/local games/madalinstuntcars/index.html"
-},
-{
-    title: "Stick Duel Battle", 
-    imgSrc: "https://lh7-us.googleusercontent.com/XHFEGITcS8r3FwLtkdt6tk15p_7WAQWz42z1klrJTvDvi3xWix20338aUsC9cnP_Z-NeGwYff8o0yX0SXj8YlzVY8ufOdldX4LcT4ybC9RYYPCtffbOkVdESo065GtRhnaRydMaBRU_xfXEnoHzeHO-lhTUcBQ",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://html5.gamedistribution.com/1d7959f081b4453498ee9916b693988c/?gd_sdk_referrer_url=https%3A%2F%2Fwww.twoplayergames.org%2Fgame%2Fstick-duel-battle")
-},
-{
     title: "Stick Man Hook", 
     imgSrc: "https://lh7-us.googleusercontent.com/p1cpa6x0ie3KK2nP6lPYZPJl6w-WF-3J2iwx5uV3rr9RRbti0ze3UBusOLp6KwUs99P5NQQ6kNxGGZDxpnP68HVu94UhjUDY7vdQJzxgzKa3cZOt-6HlzWujgCh5NaR_GnJCde51WaPmkNsp1-gnCary0fMZlQ",
     link: "/local games/stickman-hook/index.html"
-},
-{
-    title: "Papa's Pizzeria", 
-    imgSrc: "https://lh7-us.googleusercontent.com/xlAFdWg3NsJ2FnvB2HLNRhdMJayauFsKICNWfBKCv6OFymWJOgaxz8xG25d0BpRmmjNivG7AH__vC9QMM1bxSx3XBVAeLnT_GR6i3l6rUmO6v1fUBUsKZVsFmYlsv3DM-IggICm_m9cxoYKzjo3-y1Lhei1Wqg",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.coolmathgames.com/sites/default/files/public_games/49726/")
-},
-{
-    title: "Papa's Burgeria", 
-    imgSrc: "https://lh7-us.googleusercontent.com/O0mti-u5aE2j4-qe-9jTyNKAJfCHJCHTR9CziVRmw4woOyoVdLmzVgr6Wae0vK1ryx1JVICCpukRbkVgj5D3_TQUDIvlIhRElz0yysQrFq-rY6nqv_L9G7hch8-IoKsos7t4h_g9xY9HDA4wMWiDPFmnrNZahQ",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.coolmathgames.com/sites/default/files/public_games/49176/")
 },
 {
     title: "Tube Jumpers", 
@@ -381,29 +306,10 @@ const gamesData = [
     link: "/local games/tube-jumpers/index.html"
 },
 {
-    title: "Opposite Day", 
-    imgSrc: "https://lh7-us.googleusercontent.com/3MEZuozpQz1-FCmISS10dsb71FRtbq7-MFLJzXAdOIrj55BeyX7gv5jhLCx9SsWOnKP0e_kEsXp7YQgB8jOXvP6CAEs-ixviwzbJl___b1iQ0dyTejQZPODm_1SoHF-Vgve9uK1dk9le0WKuWPrma2DUmA-8XA",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.hoodamath.com/mobile/games/opposite-day/game.html?nocheckorient=1")
-},
-{
     title: "Slope Run", 
     imgSrc: "https://lh7-us.googleusercontent.com/Gb8hdT5a8UzWV0lU4WZx7LWViY8SqULyzwwjQcfBA43vo-pMKOcveBSuk0_633Rb9pkR-UHxF9ItMsdLYdLs1qrDlw0vGK-1YJrEaqExpcIDhLz-XQdAs87JnT4bop6cvD-DJ9VPiUihOi8t_pBsXIXztj5bxw",
     link: "/local games/slope-run-gamedistribution/index.html"
 },
-{
-    title: "Devil Trap Path", 
-    imgSrc: "https://lh7-us.googleusercontent.com/OIeUt1vNqld6fK6oVByterHRUNV9bz0OFKWmkJSdJUFPshHS6J-NhhnvtzZifb3xfd4SFLgcSSFoATPV-3Uc3F1V4xPHn8iUWOdxqD58vv0qTHFOlCOLW2h3xvJJSPdwTeByKhF-FHN9sb6AdOIeF24oPgcjTQ",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://storage.y8.com/y8-studio/html5/Gani/level-devil-trap-path/?key=y8&value=default")
-},
-{
-    title: "Mortal Combat", 
-    imgSrc: "https://lh7-us.googleusercontent.com/EprdGr8i7Kx5YsXuHwOx0Hov2efq_KzZtCf9pUMFBTrAQSR_96EbUc3m8snCMKg0q4p5Om_iJ6msLMBFGzfz0FBrCsdhtTeFmi1FV9nnw4EEb72M_91CegE6lvIs3o7oaX_igro6uVhUGfUIhKes8TmZBad5hw",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://oldgameshelf.com/emulator?id=L3JvbXMvbjY0L01vcnRhbCBLb21iYXQgNC9Nb3J0YWwgS29tYmF0IDQuemlw&console=n64&v=9.1")
-},
-{
-    title: "Pokemon Quetzal", 
-    imgSrc: "https://lh7-us.googleusercontent.com/y7FZT_XunMUl5bQaUk6W00FjVx5xaoGaPM2XtjNMI6xGO-8Qqasu0Ba2BASWlxadGLqAXkJlmTT5XSZ90EmtywFjsvlpOW5ksVMeP2aZlhgLT0feBQm0rNisa3VvsYAIMw94sVPWjGJD2-R6X3MsMqA-RvT3WQ",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://f.kbhgames.com/emujs/play.php?c=gba&f=PokemonQuetzalAlpha6v9.gba")
-},
+
    ];
 export { gamesData }; // Changed to named export
